667cb9b089b5758658e3bcc4bcf83bf3a1db5911
Matt Flaherty <mwflaher@alum.mit.edu>
2023-09-09 10:22:18 -0400

    url tweaking

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -2,8 +2,8 @@ require "formula"
 require_relative "../custom_download_strategy.rb"
 
 class Magi < Formula
-  homepage "https://www.gnu.org/software/wget/"
-  url "https://ftp.gnu.org/gnu/wget/wget-1.15.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
+  homepage "https://github.com/mwflaher/magi/"
+  url "https://github.com/mwflaher/magi/releases/download/magi", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
   sha256 "52126be8cf1bddd7536886e74c053ad7d0ed2aa89b4b630f76785bac21695fcd"
   
   def install
