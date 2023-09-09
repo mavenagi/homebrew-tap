b1054f5b3c631ee611beb63fa25de1eec902807f
Matt Flaherty <mwflaher@alum.mit.edu>
2023-09-09 10:06:47 -0400

    Matchy

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -1,7 +1,7 @@
 require "formula"
 require_relative "../custom_download_strategy.rb"
 
-class Wget < Formula
+class Magi < Formula
   homepage "https://www.gnu.org/software/wget/"
   url "https://ftp.gnu.org/gnu/wget/wget-1.15.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
   sha256 "52126be8cf1bddd7536886e74c053ad7d0ed2aa89b4b630f76785bac21695fcd"
