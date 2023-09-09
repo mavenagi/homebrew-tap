bbe42007afb4455c945bd7cf65d381c62e44f88a
Matt Flaherty <mwflaher@alum.mit.edu>
2023-09-09 10:45:53 -0400

    muck more with versions

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -3,7 +3,7 @@ require_relative "../custom_download_strategy.rb"
 
 class Magi < Formula
   homepage "https://github.com/mwflaher/magi/"
-  url "https://github.com/mwflaher/magi/releases/download/test/nice.png", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
+  url "https://github.com/mwflaher/magi/releases/download/0.0.0/nice.png", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
   # sha256 "52126be8cf1bddd7536886e74c053ad7d0ed2aa89b4b630f76785bac21695fcd"
   
   def install
