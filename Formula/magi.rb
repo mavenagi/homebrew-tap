d11b15818746bbe643f953a37b76ecbfac4b0998
Matt Flaherty <mwflaher@alum.mit.edu>
2023-09-10 00:14:13 -0400

    update assets

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -3,8 +3,8 @@ require_relative "../custom_download_strategy.rb"
 
 class Magi < Formula
   homepage "https://github.com/mwflaher/magi/"
-  url "https://github.com/mwflaher/magi/releases/download/0.0.0/nice.png", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
-  # sha256 "52126be8cf1bddd7536886e74c053ad7d0ed2aa89b4b630f76785bac21695fcd"
+  url "https://github.com/mwflaher/magi/releases/download/0.1.0/magi-0.1.0.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
+  sha256 "25f9c59c2f8286bce39ade5ea7b60dcf6158470d"
   
   def install
     system "./configure", "--prefix=#{prefix}"
@@ -12,6 +12,6 @@ class Magi < Formula
   end
   
   test do
-      system "#{bin}/wget --help"
+      system "#{bin}/magi --help"
   end
 end
