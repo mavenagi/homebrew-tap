43c32e416d4f0e275d9089489bf3f1015fa54274
Matt Flaherty <mwflaher@alum.mit.edu>
2023-09-10 20:01:34 -0400

    Fix install

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -7,8 +7,7 @@ class Magi < Formula
   sha256 "c0cb07d4920ef17d7e6b7d072d2d01683f96496bcfc2cedfe9d4604cfbf92628"
   
   def install
-    system "./configure", "--prefix=#{prefix}"
-    system "make", "install"
+    bin.install "magi"
   end
   
   test do
