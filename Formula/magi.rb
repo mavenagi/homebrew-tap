a20dfb2cb9144abded458d3b61428407f1219a95
Matt Flaherty <mwflaher@alum.mit.edu>
2023-09-10 20:00:21 -0400

    Fix sha

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -1,10 +1,10 @@
 require "formula"
-require_relative "../custom_download_strategy.rb"
+require_relative "../magi_download_strategy.rb"
 
 class Magi < Formula
   homepage "https://github.com/mwflaher/magi/"
   url "https://github.com/mwflaher/magi/releases/download/0.1.0/magi-0.1.0.tar.gz" 
-  sha256 "25f9c59c2f8286bce39ade5ea7b60dcf6158470d"
+  sha256 "c0cb07d4920ef17d7e6b7d072d2d01683f96496bcfc2cedfe9d4604cfbf92628"
   
   def install
     system "./configure", "--prefix=#{prefix}"
