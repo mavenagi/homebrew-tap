6d17e0a6a4c7ba2fa2fbb87e81a92773b52f68b6
Matt F <mwflaher@users.noreply.github.com>
2023-12-06 18:01:08 -0500

    Update magi.rb

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -3,7 +3,7 @@ require_relative "../magi_download_strategy.rb"
 
 class Magi < Formula
   homepage "https://github.com/mavenagi/magi/"
-  url "https://github.com/mavenagi/magi/releases/download/0.8.1/magi-0.8.1.tar.gz", :using => MagiDownloadStrategy
+  url "https://github.com/mavenagi/magi/releases/download/v0.8.1/magi-0.8.1.tar.gz", :using => MagiDownloadStrategy
   sha256 "a65522553fd26e27eeca971880f605c589e3894f829a97609486e7a25d282d38"
   
   def install
