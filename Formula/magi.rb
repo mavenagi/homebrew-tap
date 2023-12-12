e73e388ad2596906fad9dde7e94bce70cc3b4bec
Matt Flaherty <matt@mavenagi.com>
2023-12-12 17:22:48 +0000

    Bump version to v [skip ci]

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -3,8 +3,8 @@ require_relative "../magi_download_strategy.rb"
 
 class Magi < Formula
   homepage "https://github.com/mavenagi/magi/"
-  url "https://github.com/mavenagi/magi/releases/download/v0.10.1/magi-0.10.1.tar.gz", :using => MagiDownloadStrategy
-  sha256 "c98274857e2d005f39a13fd03237a4a7e42ffd8b39f2240d3c0dd4ee46e7bdbb"
+  url "https://github.com/mavenagi/magi/releases/download/v0.1.1/magi-0.1.1.tar.gz", :using => MagiDownloadStrategy
+  sha256 "abcdefg"
   
   def install
     bin.install "magi"
