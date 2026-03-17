5a791e3ba98abe36501f620d057304529e46c06a
Matt Flaherty <matt@mavenagi.com>
2026-03-17 16:54:21 -0400

    0.13.1

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -3,8 +3,8 @@ require_relative "../magi_download_strategy.rb"
 
 class Magi < Formula
   homepage "https://github.com/mavenagi/magi-cli/"
-  url "https://github.com/mavenagi/magi-cli/releases/download/v0.13.0/magi-0.13.0.tar.gz", :using => MagiDownloadStrategy
-  sha256 "ff4a542da2c6c929094bd2d00949889b3896ae94e418c0a0764ca4fce5388bc1"
+  url "https://github.com/mavenagi/magi-cli/releases/download/v0.13.0/magi-0.13.1.tar.gz", :using => MagiDownloadStrategy
+  sha256 "78d15ff13429e808204ae7d2c90efffd14d3a80ce29d5393b9c9b35d6d170b9f"
   
   def install
     bin.install "magi"
