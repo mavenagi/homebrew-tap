19b353489e67780e38ecddb54f570f0661e52352
Your Name <you@example.com>
2026-03-22 10:37:20 -0400

    updates

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -3,7 +3,7 @@ require_relative "../magi_download_strategy.rb"
 
 class Magi < Formula
   homepage "https://github.com/mavenagi/magi-cli/"
-  url "https://github.com/mavenagi/magi-cli/releases/download/v0.13.1/magi-0.14.0.tar.gz", :using => MagiDownloadStrategy
+  url "https://github.com/mavenagi/magi-cli/releases/download/v0.14.0/magi-0.14.0.tar.gz", :using => MagiDownloadStrategy
   sha256 "f8cd2e032a47f6ed08c2890e647266afbaae03818e2cf65aba87649b2d66bfbd"
   
   def install
