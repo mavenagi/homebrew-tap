cd9cf5835a280fd17fa353eaea8c1b5257d53735
brendon puntin <brendon.puntin@gmail.com>
2026-05-07 18:29:31 -0400

    chore: repoint formula from magi-cli to highlander (FORGE-4075)

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -2,9 +2,9 @@ require "formula"
 require_relative "../magi_download_strategy.rb"
 
 class Magi < Formula
-  homepage "https://github.com/mavenagi/magi-cli/"
-  url "https://github.com/mavenagi/magi-cli/releases/download/v0.18.1/magi-0.18.1.tar.gz", :using => MagiDownloadStrategy
-  sha256 "a333e9d1de4a986c6b24058ff3cdadbf1a197ef59612cb5482f8b4d017f73881"
+  homepage "https://docs.mavenagi.com"
+  url "https://github.com/mavenagi/highlander/releases/download/magi-v1.0.0/magi-1.0.0-macos.tar.gz", :using => MagiDownloadStrategy
+  sha256 "648081de4eee1f7dec83033a9c939000d5350a3338badc62b7911472a1d5255c"
   
   def install
     bin.install "magi"
