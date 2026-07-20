5d70e31c2500bebff9810bf7d4d05ab0a8e2770c
Maven AGI <info@mavenagi.com>
2026-07-20 17:18:31 +0000

    Bump version to v1.11.0 [skip ci]

--- Formula/magi.rb
+++ Formula/magi.rb
@@ -3,8 +3,8 @@ require_relative "../magi_download_strategy.rb"
 
 class Magi < Formula
   homepage "https://docs.mavenagi.com"
-  url "https://github.com/mavenagi/highlander/releases/download/magi-v1.10.0/magi-1.10.0-macos.tar.gz", :using => MagiDownloadStrategy
-  sha256 "4efdda01587238a4a59c1f7fd838550cf9b3d2702f4f0ac7230667fc6ef65b7c"
+  url "https://github.com/mavenagi/highlander/releases/download/magi-v1.11.0/magi-1.11.0-macos.tar.gz", :using => MagiDownloadStrategy
+  sha256 "2c1f6ad1186c39441e4625284dbad12047407a43188a4d63196e1e13c1756542"
   
   def install
     bin.install "magi"
